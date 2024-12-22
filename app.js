76390c64b33113d0a27225b9d025c7fa5a1c82c1
COMFORT TRAVELS <comforttravels@COMFORTs-MacBook-Air.local>
2024-12-22 13:30:23 +0530

    View engine setup

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
-// const path = require('path');
+const path = require('path');
 // const session = require('express-session');
 // const nocache = require('nocache');
 // const flash = require('connect-flash');
 const env = require('dotenv').config()
 const db = require('./config/db')
+const userRouter = require('./routes/userRouter')
 db()
 
 // app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+app.set('views',[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')])
+app.set('view engine', 'ejs');
 // app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')))
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // app.use(nocache());
 // app.use(flash());
 
@@ -41,7 +44,7 @@ db()
 // const connectDB = require('./db/connectdb');
 
 // app.use('/admin', adminRouter);
-// app.use('/user', userRouter);
+app.use('/', userRouter);
 
 // connectDB();
 
---
+++ controllers/user/userController.js
@@ -0,0 +1,23 @@
+const loadHomePage = async (req,res)=>{
+    try {
+        return res.render('home')
+    } catch (error) {
+        console.log('Home Page not found');
+        res.status(500).send('Server error')
+        
+    }
+}
+
+const pageNotFound = async (req,res)=>{
+    try {
+        res.render('page-404')
+    } catch (error) {
+        res.redirect('/pageNotFound')
+    }
+}
+
+
+module.exports ={
+    loadHomePage,
+    pageNotFound
+}
